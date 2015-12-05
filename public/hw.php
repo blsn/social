f8b20d0468aa73b7d9770eb771c545812e82934d
vagrant <blsn2000@gmail.com>
2015-12-05 10:32:43 -0500

    6th commit

--- public/hw.php
+++ public/hw.php
@@ -1,3 +1,12 @@
+<h3>Mary Jatty Embedded</h3>
+
 <?php
-echo 'hello world';
+	echo 'hello world';
+    $baseUrl = 'http://edchant.tk:8080/';
+    $src = $baseUrl;
 ?>
+
+<audio autoplay controls>
+  <source src="<?php echo($src); ?>" type='audio/wav' autoplay>
+  Your browser does not support the audio tag.
+</audio> <?php
